e0c96dd56bd2300f44bab77e51625a72c64391cb
LuismiSAFA <56118701+LuismiSAFA@users.noreply.github.com>
2021-09-29 19:34:38 +0200

    wowowo

--- Proy3TFX2021/src/controlador/Proy3TFX.java
+++ Proy3TFX2021/src/controlador/Proy3TFX.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javax.swing.UIManager;
